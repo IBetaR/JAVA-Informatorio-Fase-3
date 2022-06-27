11397291354a78481e202676fede03ac5bd06af3
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-06-26 22:50:23 -0300

    E4 y E5

---
+++ E4/E4.java
@@ -0,0 +1,7 @@
+package E4;
+
+public class E4 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ E5/E5.java
@@ -0,0 +1,7 @@
+package E5;
+
+public class E5 {
+    public static void main(String[] args) {
+        
+    }
+}
