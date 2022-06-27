c5fe084c4fc25497a7f13429e69f7063ea22b597
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-06-26 22:44:48 -0300

    Ejercicio3

--- E3/E3.java
+++ E3/E3.java
@@ -1,7 +1,20 @@
 package E3;
 
+import java.util.List;
+
 public class E3 {
     public static void main(String[] args) {
+
+        List<String> palabras = List.of("Batman", "Aq", "FLash", "Batgirl", "WondeW", "brainiac");
+        System.out.println(palabras);
+    
+        Long cantidadDeletrab = palabras.stream()
+
+            .map(String::toLowerCase)
+            .filter(palabra -> palabra.startsWith("b"))
+            .count();
+            //.forEach(System.out::println);
+        System.out.println(cantidadDeletrab);
         
     }
 }
